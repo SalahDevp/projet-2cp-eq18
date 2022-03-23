6f8360cb1dbe8aadb921fce3e152c751dacd130c
hamada <hamadahambli@gmail.com>
2022-03-23 14:07:35 +0000

    added move point

--- src/pages/Paint.js
+++ src/pages/Paint.js
@@ -13,58 +13,34 @@ const HEIGHT = 500,
   WIDTH = 1000,
   UNIT = 20;
 
+const firstshape = new Shape({ x: 40, y: 60 });
+firstshape.points.push({ x: 60, y: 160 }, { x: 80, y: 120 });
 const Paint = () => {
   const [drawing, setDrawing] = useState(false);
-  const [shapes, setShapes] = useState([]);
+  const [shapes, setShapes] = useState([firstshape]);
   const [current, setCurrent] = useState({});
   const [line, setLine] = useState();
   const canvasRef = useRef(null);
 
   const handleMouseDown = (event) => {
-    //start drawing
-    setDrawing(true);
-    //get mouse coordinates
     const { x: mouseX, y: mouseY } = getMousePos(canvasRef, event);
-    // get closest grid point to mouse coord
     const { x, y } = getGridPos(mouseX, mouseY, UNIT);
-    setLine({ x1: x, y1: y, x2: x, y2: y });
-    //check if the point already belongs to a shape
-    let { shape, pointIndex } = getShapeFromPoint(shapes, x, y);
-    //if the shape isn't a "polygone" and the selected point is the first or the last one
-    if (
-      shape &&
-      !shape.polygone &&
-      (pointIndex === 0 || pointIndex === shape.points.length - 1)
-    )
+    const { shape, pointIndex } = getShapeFromPoint(shapes, x, y);
+    if (shape) {
+      setDrawing(true);
       setCurrent({ shape, pointIndex });
-    //else we create a new shape
-    else {
-      shape = new Shape({ x, y });
-      setShapes((prv) => [...prv, shape]);
-      setCurrent({ shape, pointIndex: 0 });
     }
   };
   const handleMouseMove = (event, lastPoint) => {
     if (!drawing) return;
-    const { x: mouseX, y: mouseY } = getMousePos(canvasRef, event);
-    const { x, y } = lastPoint
-      ? getGridPos(mouseX, mouseY, UNIT)
-      : { x: mouseX, y: mouseY };
-    setLine((prv) => ({ ...prv, x2: x, y2: y }));
-    //updating line bcs setLine is asyn (problem in handleMouseUp)
-    if (lastPoint) {
-      line.x2 = x;
-      line.y2 = y;
-    }
+    const { x, y } = getMousePos(canvasRef, event);
+    current.shape.points[current.pointIndex] = { x, y };
+    setLine({ x, y });
   };
   const handleMouseUp = (event) => {
-    //set the line
-    handleMouseMove(event, true);
-    //add the new point to the current shape
-    if (current.pointIndex === current.shape.points.length - 1)
-      current.shape.points.push({ x: line.x2, y: line.y2 });
-    else current.shape.points.unshift({ x: line.x2, y: line.y2 });
-    //stop drawing
+    const { x: mouseX, y: mouseY } = getMousePos(canvasRef, event);
+    const { x, y } = getGridPos(mouseX, mouseY, UNIT);
+    current.shape.points[current.pointIndex] = { x, y };
     setDrawing(false);
   };
   const drawLine = (context, line) => {
@@ -86,7 +62,7 @@ const Paint = () => {
     clearCanvas(context, WIDTH, HEIGHT, UNIT);
     shapes.forEach((shape) => shape.draw(context));
     if (line?.x1) drawLine(context, line);
-  }, [shapes, line]);
+  }, [shapes, line, drawing]);
   return (
     <>
       <Canvas
