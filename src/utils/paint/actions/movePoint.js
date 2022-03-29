2f4b3336c229b7c92afb7c9c010b2525cb93e09d
hamada <hamadahambli@gmail.com>
2022-03-29 09:46:05 +0000

    MoveTwoPoints Finalist

--- src/utils/paint/actions/movePoint.js
+++ src/utils/paint/actions/movePoint.js
@@ -35,22 +35,6 @@ export const handleMouseMove = (event, state, lastPoint) => {
   state.setLine({ x, y });
 };
 export const handleMouseUp = (event, state) => {
-  const getLineEq = (point1, point2) => {
-    //this function calculate a line equation
-    let m = (point2.y - point1.y) / (point2.x - point1.x); // m almail ta3 lmoosta9iim
-    let b =
-      m !== Infinity || m !== -Infinity ? point2.y - m * point2.x : point1.x; // b howa noo9tat ta9atao3 lmoosta9im m3a mi7war ltaratib (y = ax+b)
-    return { m, b };
-  };
-  const pointBelongsToLine = (lineEq, point) => {
-    //this function checks if a point is a part of a line given
-    if (lineEq.m !== Infinity || lineEq.m !== -Infinity)
-      return point.y === lineEq.m * point.x + lineEq.b;
-    else return point.x === lineEq.b;
-  };
-  // linelength ta7ssab ettol ta3 l9it3a
-  const lineLength = (p1, p2) =>
-    Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
   if (!state.drawing) return;
   const { x: mouseX, y: mouseY } = getMousePos(state.canvasRef, event);
   const { x, y } = getGridPos(mouseX, mouseY, UNIT);
@@ -81,7 +65,10 @@ export const handleMouseUp = (event, state) => {
     }
   }
 
-  if (checkIfPointInShapeSegment({ x, y }, state.shapes)) {
+  if (
+    checkIfPointInShapeSegment({ x, y }, state.shapes) ||
+    rahMoghtasib(state.current.shape, state.current.pointIndex)
+  ) {
     state.current.shape.points[state.current.pointIndex].x = state.current.x;
     state.current.shape.points[state.current.pointIndex].y = state.current.y;
   }
