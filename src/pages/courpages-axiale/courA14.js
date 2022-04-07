5f8ae3059107088e43d5e6e1204601e22a54c948
hichem <kh_boulouza@esi.dz>
2022-04-07 15:59:50 +0100

    green-finish

--- src/pages/courpages-axiale/courA14.js
+++ src/pages/courpages-axiale/courA14.js
@@ -8,7 +8,7 @@ const CourA14 = () => {
              
              <div className='ml-6 mt-4'>
                  <p className='  text-xl font-normal' >
-                 Le symétrique d’un angle par rapport à une droite (d)  est <span className='text-ltr-cr'> un angle de même mesure</span>.
+                 Le symétrique d’un angle par rapport à une droite <span className='text-ltr-cr'>(d)</span> est <span className='text-ltr-cr'> un angle de même mesure</span>.
                  </p>
                  <ul className='ml-10 mt-2 list-disc  text-xl font-normal'>
                    <li>L’angle <span className='text-ltr-cr'>BÂC</span> et l’angle <span className='text-ltr-cr'>B’Â’C’</span> ont la même mesure.</li>
